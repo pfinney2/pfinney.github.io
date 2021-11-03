aaf4e2d7779001b7cc594d81103829dd5fed7e7c
phfinney4 <pf@pfinney.com>
2021-11-03 10:30:55 -0500

    Add files via upload

--- Code/throwaway/script.js
+++ Code/throwaway/script.js
@@ -1,26 +1,8 @@
 jQuery(document).ready(function($){
 	var isLateralNavAnimating = false;
-	var position = 'up';
 	
 	//open/close lateral navigation
 	$('.cd-nav-trigger').on('click', function(event){
-		
-		$('html, body').animate({ scrollTop: 0 }, 'slow');
-		if( position = 'up' ) {
-
-			position = 'down';
-			console.log(position);
-		} else if ( position = 'down' ) {
-			position = 'up';
-			console.log(position);
-		} else {
-
-			position = 'up'
-
-			console.log(position);
-		}
-
-
 		event.preventDefault();
 		//stop if nav animation is running 
 		if( !isLateralNavAnimating ) {
@@ -32,19 +14,5 @@ jQuery(document).ready(function($){
 				isLateralNavAnimating = false;
 			});
 		}
-
-		console.log($( document ).height());
-
 	});
-
-
-	$("a[href='#contact']").click(function() {
-		$("html, body").animate({ scrollTop: $(document).height() }, "slow");
-
-		return false;
-
-	});
-
-
-
 });
