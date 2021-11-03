9b7f1be4a2f1f33a21c359a2b9d046df076e788e
phfinney4 <pf@pfinney.com>
2021-11-03 08:11:06 -0500

    Add files via upload

---
+++ Code/script/menu_drop_script.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($){
+	var isLateralNavAnimating = false;
+	
+	//open/close lateral navigation
+	$('.cd-nav-trigger').on('click', function(event){
+		
+		$('html, body').animate({ scrollTop: 0 }, 'slow');
+
+		event.preventDefault();
+		//stop if nav animation is running 
+		if( !isLateralNavAnimating ) {
+			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+			
+			$('body').toggleClass('navigation-is-open');
+			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				//animation is over
+				isLateralNavAnimating = false;
+			});
+		}
+	});
+
+	$("a[href='#contact']").click(function() {
+		$("html, body").animate({ scrollTop: $(document).height() }, "slow");
+		return false;
+	});
+
+
+
+});
